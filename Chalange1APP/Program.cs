8651d5f135bb4d464169ca2c45b3d1dbeb71fdb5
fapomobile <a.terwinski@o2.pl>
2023-04-03 13:37:30 +0200

    Zadanie 4

--- Chalange1APP/Program.cs
+++ Chalange1APP/Program.cs
@@ -1,2 +1,29 @@
 ﻿// To są moje początki
-Console.WriteLine("Hello, Adam!Jestem gotowy do pracy");
+using System.ComponentModel.Design;
+
+var name = "EWA";
+var age = 13;
+var sex1 = "mężczyzna";
+var sex2 = "kobieta";
+string tinager = "Niepełnoletni";
+if (age == 33)
+{
+    if (name == "EWA")
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+}
+else if (age < 18)
+{
+    if (sex1 == "mężczyzna")
+    {
+        Console.WriteLine("jestem niepełnoletni Adam");
+    }
+}
+else if (age < 30)
+{
+    if (sex2 == "kobieta") ;
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
