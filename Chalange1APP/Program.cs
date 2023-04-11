79cc5b8277765b6fe369d19a8d1b979599e995e6
fapomobile <a.terwinski@o2.pl>
2023-04-11 14:28:50 +0200

    ćwiczenie 4

--- Chalange1APP/Program.cs
+++ Chalange1APP/Program.cs
@@ -2,15 +2,19 @@
 using System.ComponentModel.Design;
 
 var name = "EWA";
-var age = 13;
+var age = 30;
 var sex1 = "mężczyzna";
 var sex2 = "kobieta";
-string tinager = "Niepełnoletni";
-if (age == 33)
+
+if (age == 33 && sex2 == "kobieta")
 {
     if (name == "EWA")
     {
         Console.WriteLine("Ewa lat 33");
+    }
+    else
+    {
+        Console.WriteLine("To jest jakaś kobieta");
     }
 }
 else if (age < 18)
@@ -18,6 +22,10 @@ else if (age < 18)
     if (sex1 == "mężczyzna")
     {
         Console.WriteLine("jestem niepełnoletni Adam");
+    }
+    else
+    {
+        Console.WriteLine("To może być kobieta");
     }
 }
 else if (age < 30)
