0afb440b2011b5fce71cfbaf5b3ee2139ea42504
fapomobile <a.terwinski@o2.pl>
2023-04-15 22:34:58 +0200

    ćwiczenie 5 próby

--- Chalange1APP/Program.cs
+++ Chalange1APP/Program.cs
@@ -1,25 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more informatio
-int number1 = 5;
-int number2 = 10;
+﻿int number = 8336;
+string numer = number.ToString();
+var charLiczba = numer.ToArray();
 
+Console.WriteLine("Wyniki dla liczby: " + numer);
+Console.WriteLine("Ilość cyfr = " + charLiczba.Length);
 
-var name = "adam";
-var age = 50;
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-if (age > 55)
+foreach (char letter in charLiczba)
 {
-    if (name == "adam")
-    {
-        Console.WriteLine("jESTEM Adamem lub mam mniej niż 60 at");
+    if(letter == '0')
+    { 
+        
+        Console.WriteLine("0 => " + counter0);
+        counter0++;
+        
     }
-    else if (age > 40)
+    else if(letter == '1')
     {
-        Console.WriteLine("jESTEM W wieku 40 lat");
+        
+        Console.WriteLine("1 => " + counter1);
+        counter1++;
     }
-}
-else if (age < 30);
-{
-    Console.WriteLine("jESTEM W wieku 30 lat");
+    else if(letter == '2')
+    {
+        
+        Console.WriteLine("2 => " + counter3);
+        counter2++;
+    }
+    else if(letter == '3')
+    {
+        
+        Console.WriteLine(" => " + counter3);
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        
+        Console.WriteLine("4 => ");
+        counter4++;
+    }
+    else if(letter == '5')
+    {
+        
+        Console.WriteLine("5 => ");
+        counter5++;
+    }
+    else if(letter == '6')
+    {
+        
+        Console.WriteLine("6 => " + counter6);
+        counter6++;
+    }
+    else if(letter == '7')
+    {
+        
+        Console.WriteLine("7 => ");
+        counter7++;
+    }
+    else if(letter == '8')
+    {
+        
+        Console.WriteLine("8 => " + counter8);
+        counter8++;
+    }
+    else if(letter == '9')
+    {
+        
+        Console.WriteLine("9 => ");
+        counter9++;
+    }
+    Console.WriteLine(letter);
 }
 // OPERATORY RELACYJNE 
 // ==   równe
